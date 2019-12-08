98e2101d4f38666beef602d01dac80d6f2e37f71
megadose <51822495+megadose@users.noreply.github.com>
2019-12-08 10:48:21 +0100

    Update Fl0wj0b.py

--- Fl0wj0b.py
+++ Fl0wj0b.py
@@ -25,7 +25,7 @@ def doublon(dictdata,dictdata2):
                 tel = i['Telephone']
                 check = tel in str(dict_datafinal) in str(dict_datafinal)
             else:
-                dict_datafinal.append(dict(Nom=i["Nom"], Adresse=i['Adresse'].lower().replace(",",""),CodePostal=i['CodePostal'], Ville=i['Ville'], Telephone=i["Nom"]+" ne désire pas publier son numéro de téléphone."))
+                dict_datafinal.append(dict(Nom=i["Nom"], Adresse=i['Adresse'].lower().replace(",",""),CodePostal=i['CodePostal'], Ville=i['Ville'], Telephone=i["Nom"]+" ne desire pas publier son numero de telephone"))
             if(check==False):
                 dict_datafinal.append(dict(Nom=i["Nom"], Adresse=i['Adresse'].lower().replace(",",""),CodePostal=i['CodePostal'], Ville=i['Ville'], Telephone=i['Telephone']))
     else :
